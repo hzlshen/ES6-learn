721dc1dbf6a72a3573e779660aa987aafd796645
hzlshen <m15277968105@163.com>
2017-10-24 18:11:00 +0800

    编程风格结束，略略略~

--- let-and-const.js
+++ let-and-const.js
@@ -270,14 +270,147 @@ if (true)
 //块级将两个语句封装到一起 但是在块级之外是没办法得到t的值 因为块级作用域不返回值
 //除非t是全局变量
 //现在 有一个操作 让这个块级作用域有返回值 办法就是在块级作用域之前加do
-let x = do{
-    let t = f();
-    t * t+1;
-}
+// let x = do{
+//     let t = f();
+//     t * t+1;
+// }
 //变量x 会得到整个块级作用域的返回值
 
 //const命令
 //const声明一个只读的常量。一旦声明，常量的值就不能改变。
+const PI = 3.1415926;
+PI;  //3.1415926
+
+PI = 3; //报错
+//修改常量会报错
+//const声明的变量不得改变值 这意味着 const一旦声明变量 就必须立即初始化 不能留到以后赋值
+const foo;
+//SyntaxError: Missing initializer in const declaration
+//对const来说 只声明不赋值 只有报错一途
+
+//const的作用域和let命令相同 只在声明所在的块级作用域内有效
+if (true){
+    const MAX = 5;
+}
+MaX// Uncaught ReferenceError: MAX is not defined
+
+//const 也不能声明提前 只能在声明后使用
+if(true){
+    console.log(aa);//ReferenceError
+    const aa = 3;
+}
+
+//const声明的常量 也和let一样不可重复声明
+var message = 'Hello';
+let message = 35;
+
+// 以下两行都会报错
+const message = "good";
+const age = 32;
+
+//对于复合类型的变量，变量名不指向数据，而是指向数据所在的地址。
+//const命令只是保证变量名指向的地址不变，并不保证该地址的数据不变，
+// 所以将一个对象声明为常量必须非常小心。
+
+const foo = {};
+foo.prop = 123;
+foo.prop
+//123
+
+//地址指向一个对象 地址不可变 对象本身可变 可以为其添加属性
+foo ={}// TypeError: "foo" is read-only
+
+const a = [];
+a.push('Hello');//可执行
+a.length = 0;  //可执行
+a = ['Dave'];  //报错
+
+//常量a是个数组 数组本身可写 但要将另一个数组赋值给a 就会报错
+//如果真的想将对象冻结 应该使用Object.freeze方法
+const foo = Object.freeze({});
+
+// 常规模式时，下面一行不起作用；
+// 严格模式时，该行会报错
+foo.prop = 123;
+
+//冻结对象的属性
+var constantize = (obj) => {
+    Object.freeze(obj);
+    Object.keys(obj).forEach( (key, value) => {
+        if ( typeof obj[key] === 'object' ) {
+            constantize( obj[key] );
+        }
+    });
+};
+
+//ES5只有两种声明变量的方法：var命令和function命令。
+// ES6除了添加let和const命令，后面章节还会提到，
+// 另外两种声明变量的方法：import命令和class命令。所以，ES6一共有6种声明变量的方法。
+window.a = 1;
+a  //1
+
+a = 2;
+window.a //2
+//顶层对象的属性赋值与全局变量的赋值，是同一件事。
+//顶层对象的属性赋值与全局变量挂钩 被认为是JavaScript语言最大的设计败笔之一
+//这样的设计带来了几个很大的问题，
+// 首先是没法在编译时就报出变量未声明的错误，只有运行时才能知道（因为全局变量可能是顶层对象的属性创造的，而属性的创造是动态的）；
+// 其次，程序员很容易不知不觉地就创建了全局变量（比如打字出错）；
+// 最后，顶层对象的属性是到处可以读写的，这非常不利于模块化编程。
+// 另一方面，window对象有实体含义，指的是浏览器的窗口对象，顶层对象是一个有实体含义的对象，也是不合适的。
+
+//ES6为了改变这一点，一方面规定，为了保持兼容性，var命令和function命令声明的全局变量，依旧是顶层对象的属性；
+// 另一方面规定，let命令、const命令、class命令声明的全局变量，不属于顶层对象的属性。
+// 也就是说，从ES6开始，全局变量将逐步与顶层对象的属性脱钩。
+
+var a  = 3;
+// 如果在Node的REPL环境，可以写成global.a
+// 或者采用通用方法，写成this.a
+window.a // 1
+
+let b = 1;
+window.b // undefined
+//全局变量a由var命令声明，所以它是顶层对象的属性；
+// 全局变量b由let命令声明，所以它不是顶层对象的属性，返回undefined。
+
+//很难找到一种方法，可以在所有情况下，都取到顶层对象。下面是两种勉强可以使用的方法。
+
+// 方法一
+(typeof window !== 'undefined'
+    ? window
+    : (typeof process === 'object' &&
+typeof require === 'function' &&
+typeof global === 'object')
+    ? global
+    : this);
+
+// 方法二
+var getGlobal = function () {
+    if (typeof self !== 'undefined') { return self; }
+    if (typeof window !== 'undefined') { return window; }
+    if (typeof global !== 'undefined') { return global; }
+    throw new Error('unable to locate global object');
+};
+//现在有一个提案，在语言标准的层面，引入global作为顶层对象。也就是说，在所有环境下，global都是存在的，都可以从它拿到顶层对象。
+
+//垫片库system.global模拟了这个提案，可以在所有环境拿到global。
+
+// CommonJS的写法
+require('system.global/shim')();
+
+// ES6模块的写法
+import shim from 'system.global/shim'; shim();
+//上面代码可以保证各种环境里面，global对象都是存在的。
+
+// CommonJS的写法
+var global = require('system.global')();
+
+// ES6模块的写法
+import getGlobal from 'system.global';
+const global = getGlobal();
+
+
+
 
 
 

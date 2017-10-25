e73bf290593bf805d2ae64dc07129c1efd43d699
hzlshen <m15277968105@163.com>
2017-10-25 18:39:39 +0800

    get day

--- Array.js
+++ Array.js
@@ -23,3 +23,54 @@ let ps = document.querySelectorAll('p');
 Array.from(ps).forEach(function(p){
     console.log(p);
 })
+
+// arguments对象
+function foo() {
+    var args = Array.from(arguments);
+    // ...
+}
+
+//只要是部署了Iterator接口的数据结构，Array.from都能将其转为数组。
+Array.from('hello');
+//['h', 'e', 'l', 'l', 'o']
+
+let namesSet = new Set(['a','b']);
+Array.from(namesSet);
+//字符串和Set结构都具有Iterator接口，因此可以被Array.from转为真正的数组。
+
+Array.from(1,2,3);
+
+//扩展运算符（...）也可以将某些数据结构转为数组。
+//arguments
+function foo() {
+    var args =  [...arguments];
+}
+Array.from({ length: 3 });
+// [ undefined, undefined, undefined ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

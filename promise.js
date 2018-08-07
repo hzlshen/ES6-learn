4fd0152885499a960ff51a12ac7f4a882090f37e
hzlshen <m15277968105@163.com>
2018-08-07 23:48:28 +0800

    getJSON

--- promise.js
+++ promise.js
@@ -8,3 +8,33 @@ var promise = new Promise(function(resolve, reject) {
       reject(error);
     }
   });
+
+  var getJSON = function(url) {
+  var promise = new Promise(function(resolve, reject){
+    var client = new XMLHttpRequest();
+    client.open("GET", url);
+    client.onreadystatechange = handler;
+    client.responseType = "json";
+    client.setRequestHeader("Accept", "application/json");
+    client.send();
+
+    function handler() {
+      if (this.readyState !== 4) {
+        return;
+      }
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+  });
+
+  return promise;
+};
+
+getJSON("/posts.json").then(function(json) {
+  console.log('Contents: ' + json);
+}, function(error) {
+  console.error('出错了', error);
+});
